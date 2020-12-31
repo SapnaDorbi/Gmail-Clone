4bf24fdf3d451fe8db73bca5ecfbd2fe0e3e26e8
SapnaDorbi <savydorbi@gmail.com>
2020-12-31 22:56:28 +0530

    worked on sidebar and header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <h1>Lets build gmail</h1>
+      <Sidebar />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Header.css';
 import MenuIcon from '@material-ui/icons/Menu';
-import { IconButton } from '@material-ui/core';
+import { Avatar,IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header () {
     return (
@@ -10,12 +14,23 @@ function Header () {
                 <IconButton>
                     <MenuIcon />
                 </IconButton>
+                <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="" />
             </div>
+
             <div className="header__middle">
-
+                <SearchIcon />
+                <input placeholder="Search mail" type="text" />
+                <ArrowDropDownIcon className="header__inputCaret" />
             </div>
-            <div className="header__right">
 
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
             </div>
         </div>
     );
